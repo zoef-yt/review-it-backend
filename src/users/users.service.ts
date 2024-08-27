b080c916af15829d2ee1155a91024196fbe6b29a
Zoef <shaikhzoef36@gmail.com>
2024-08-27 14:41:25 +0530

    fix validate

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -20,9 +20,7 @@ export class UsersService {
     }
     return true;
   }
-  async deleteUser(email: string) {
-    return await this.userModel.findByIdAndDelete(email).exec();
-  }
+
   async createUser(createUserDto: CreateUserDto) {
     const { email, username, password } = createUserDto;
     const hashedPassword = await bcrypt.hash(password, 10);
