6177d13fa010360b60ea7858218c5941c6c84948
Zoef <shaikhzoef36@gmail.com>
2024-09-09 15:49:32 +0530

    change get single game

--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -1,15 +1,14 @@
 import { Controller, Get, Param } from '@nestjs/common';
 
 import { GamesService } from './games.service';
-// import { CreateGameDto } from './dto/create-game.dto';
 import { Game } from './schema/games.schema';
 
 @Controller('games')
 export class GamesController {
   constructor(private readonly gamesService: GamesService) {}
 
-  @Get(':gameId')
-  async GetSingleGame(@Param('gameId') gameId: string): Promise<Game> {
-    return this.gamesService.findOne(gameId);
+  @Get(':gameSlug')
+  async GetSingleGame(@Param('gameSlug') gameSlug: string): Promise<Game> {
+    return this.gamesService.findOneByGameSlug(gameSlug);
   }
 }
